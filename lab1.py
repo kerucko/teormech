490c1a087095bfe1f8bf6c158969c0e0995a1eca
kerucko <cykinpavel@gmail.com>
2022-09-21 22:26:56 +0300

    first commit

---
+++ lab1.py
@@ -0,0 +1,94 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import sympy as sp
+import math
+
+
+def Rotation(X, Y, alpha):
+    RX = X * np.cos(alpha) - Y * np.sin(alpha)
+    RY = X * np.sin(alpha) + Y * np.cos(alpha)
+    return RX, RY
+
+
+def animation(i):
+    P.set_data(X[i], Y[i])
+    VLine.set_data([X[i], X[i] + VX[i]], [Y[i], Y[i] + VY[i]])
+    ALine.set_data([X[i], X[i] + AX[i]], [Y[i], Y[i] + AY[i]])
+    RArrowVX, RArrowVY = Rotation(ArrowX, ArrowY, math.atan2(VY[i], VX[i]))
+    VArrow.set_data(RArrowVX + X[i] + VX[i], RArrowVY + Y[i] + VY[i])
+    RArrowAX, RArrowAY = Rotation(ArrowX, ArrowY, math.atan2(AY[i], AX[i]))
+    AArrow.set_data(RArrowAX + X[i] + AX[i], RArrowAY + Y[i] + AY[i])
+    # NLine.set_data([X[i], (X[i] + Y[i]) * k[i]], [Y[i], (Y[i] - X[i]) * k[i]])
+    # print(k[i])
+    return P, VLine, ALine, VArrow, AArrow
+
+
+t = sp.Symbol('t')
+R = 10
+S = 5
+
+# r = sp.cos(6 * t)
+# fi = t + 0.2 * sp.cos(3 * t)
+
+r = 2 + sp.sin(6 * t)
+fi = 6.5 * t + 1.2 * sp.cos(6 * t)
+
+x = r * sp.cos(fi)
+y = r * sp.sin(fi)
+
+Vx = sp.diff(x, t)
+Vy = sp.diff(y, t)
+Ax = sp.diff(Vx, t)
+Ay = sp.diff(Vy, t)
+
+T = np.linspace(0, 15, 1000)
+
+X = np.zeros_like(T)
+Y = np.zeros_like(T)
+VX = np.zeros_like(T)
+VY = np.zeros_like(T)
+AX = np.zeros_like(T)
+AY = np.zeros_like(T)
+# k = np.zeros_like(T)
+
+for i in np.arange(len(T)):
+    X[i] = sp.Subs(x, t, T[i])
+    Y[i] = sp.Subs(y, t, T[i])
+    VX[i] = sp.Subs(Vx, t, T[i])
+    VY[i] = sp.Subs(Vy, t, T[i])
+    AX[i] = sp.Subs(Ax, t, T[i])
+    AY[i] = sp.Subs(Ay, t, T[i])
+    # V = np.array(VX[i], VY[i])
+    # A = np.array(AX[i], AY[i])
+    # Vn = np.linalg.norm(V)
+    # An = np.linalg.norm(A)
+    # norms = Vn * An
+    # dot_pr = V.dot(A)
+    # sin = np.sin(np.arccos(dot_pr / norms))
+    # k[i] = Vn ** 2 / (An * sin)
+    # if a == 0 or sin == 0:
+    #     print(An, sin, dot_pr, norms)
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(1, 1, 1)
+ax1.axis('equal')
+ax1.set(xlim=[-R, R], ylim=[-R, R])
+
+ax1.plot(X, Y)
+
+P, = ax1.plot(X[0], Y[0], marker='o')
+VLine, = ax1.plot([X[0], X[0] + VX[0]], [Y[0], Y[0] + VY[0]], 'r')
+ALine, = ax1.plot([X[0], X[0] + AX[0]], [Y[0], Y[0] + AY[0]], 'y')
+ArrowX = np.array([-0.2 * S, 0, -0.2 * S])
+ArrowY = np.array([0.1 * S, 0, -0.1 * S])
+RArrowVX, RArrowVY = Rotation(ArrowX, ArrowY, math.atan2(VY[0], VX[0]))
+VArrow, = ax1.plot(RArrowVX + X[0] + VX[0], RArrowVY + Y[0] + VY[0], 'r')
+RArrowAX, RArrowAY = Rotation(ArrowX, ArrowY, math.atan2(AY[0], AX[0]))
+AArrow, = ax1.plot(RArrowAX + X[0] + AX[0], RArrowAY + Y[0] + AY[0], 'y')
+# NLine, = ax1.plot([X[0], (X[0] + Y[0]) * k[0]], [Y[0], (Y[0] - X[0]) * k[0]], 'black')
+
+anim = FuncAnimation(fig, animation, frames=1000, interval=2, blit=True, repeat=True)
+
+plt.show()
